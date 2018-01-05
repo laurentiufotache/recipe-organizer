7eafff4dd85dcf24130c82002493932065e134e4
Marius Nicolae <marius.a.nicolae@outlook.com>
2018-01-05 23:31:13 +0200

    and so it begins ...

---
+++ app/src/main/java/com/example/mariu/recipe_organizer/DataItemAdapter.java
@@ -0,0 +1,47 @@
+package com.example.mariu.recipe_organizer;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Mariu on 1/5/2018.
+ */
+
+public class DataItemAdapter extends ArrayAdapter<DataItem> {
+
+    List<DataItem> mDataItems;
+    LayoutInflater mInflater;
+
+    public DataItemAdapter(@NonNull Context context, @NonNull List<DataItem> objects) {
+        super(context, R.layout.list_item, objects);
+        mDataItems = objects;
+        mInflater = LayoutInflater.from(context);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView txtName = (TextView) convertView.findViewById(R.id.itemNameText);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+
+        DataItem item = mDataItems.get(position);
+        txtName.setText(item.getItemName());
+        imageView.setImageResource(R.drawable.apple_pie);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/mariu/recipe_organizer/MainActivity.java
+++ app/src/main/java/com/example/mariu/recipe_organizer/MainActivity.java
@@ -2,18 +2,56 @@ package com.example.mariu.recipe_organizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView text1;
+   // private TextView text1;
+    List<DataItem> dataItemList = DataProvider.dataItemList;
+    List<String> itemNames = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text1 = (TextView) findViewById(R.id.out);
-        text1.setText("Ready to code!");
+        //testing the output
+//        DataItem item =
+//                new DataItem(null, "My menu item", "A Category", "A description", 1, 9.95,
+//                "C:\\Users\\Mariu\\Desktop\\RecipeOrganizer\\assets\\Apple_pie.jpg");
+
+//        text1 = (TextView) findViewById(R.id.out);
+//        text1.setText("");
+//
+//        //sorting items A-Z
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem t1, DataItem t2) {
+                return t1.getItemName().compareTo(t2.getItemName());
+            }
+        });
+
+        //shows all items created in DataProvider Class
+//        for (DataItem item: dataItemList) {
+//            //text1.append(item.getItemName() + "\n");
+//            itemNames.add(item.getItemName());
+//        }
+//        Collections.sort(itemNames);
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+//                this, android.R.layout.simple_list_item_1, itemNames);
+
+        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
+        ListView listView = findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
 
     }
 }
