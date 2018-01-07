b6f7af30b82084986590d7aa4771ac5c0202d108
Marius Nicolae <marius.a.nicolae@outlook.com>
2018-01-07 02:44:15 +0200

    added sign in and settings functions to the app

--- app/src/main/java/com/example/mariu/recipe_organizer/MainActivity.java
+++ app/src/main/java/com/example/mariu/recipe_organizer/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.mariu.recipe_organizer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.widget.Toast;
+import android.widget.*;
+import android.*;
+import com.example.mariu.recipe_organizer.DataItem;
+import com.example.mariu.recipe_organizer.DataProvider;
+import android.*;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-   // private TextView text1;
-    List<DataItem> dataItemList = DataProvider.dataItemList;
-    List<String> itemNames = new ArrayList<>();
+   private static final int SIGNIN_REQUEST = 1001;
+   public static final String MY_GLOBAL_PREFS = "my_global_prefs";
+   List<DataItem> dataItemList = DataProvider.dataItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +41,40 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.rvItems);
         recyclerView.setAdapter(adapter);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_signin:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivityForResult(intent, SIGNIN_REQUEST);
+                return true;
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, PrefsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST) {
+            String email = data.getStringExtra(LoginActivity.EMAIL_KEY);
+            Toast.makeText(this, "You signed in as " + email, Toast.LENGTH_SHORT).show();
+            SharedPreferences.Editor editor =
+                    getSharedPreferences(MY_GLOBAL_PREFS, MODE_PRIVATE).edit();
+            editor.putString(LoginActivity.EMAIL_KEY, email);
+            editor.apply();
+
+        }
     }
 }
